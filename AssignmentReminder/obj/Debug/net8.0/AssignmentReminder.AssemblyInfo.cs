c13f061c34fe6ce42d6383961230852cc46ba6e7
Genevieve <awavieve@gmail.com>
2025-02-23 16:22:30 -0500

    Remove no restore and add .dockerignore

--- AssignmentReminder/obj/Debug/net8.0/AssignmentReminder.AssemblyInfo.cs
+++ AssignmentReminder/obj/Debug/net8.0/AssignmentReminder.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssignmentReminder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b48a12f08a6a26b92c9436923b41b9eb421e5fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3898434fc926cd9927dfcb18651ed86cb79e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssignmentReminder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssignmentReminder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
